e5ea3692e02ab389f4e29f9692a813cad7f2812e
IgnatioFerreira <ignatio.ferreira@gmail.com>
2021-07-03 15:19:25 +0200

    visuals progress

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -15,25 +15,43 @@ export default class SortingVisualizer extends React.Component {
     resetArray() {
         const array = [];
         for (let i = 0; i < 200; i++) {
-            array.push(randomIntFromInterval(5, 750));
+            array.push(randomIntFromInterval(5, 600));
         }
         this.setState({ array });
     }
 
+    mergeSort(){}
+    quickSort(){}
+    heapSort(){}
+    bubbleSort(){}
     render() {
         const { array } = this.state;
 
         return (
-            <div className="array-container">
-                {array.map((value, idx) => (
-                    <div
-                        className="array-bar"
-                        key={idx}
-                        style={{height: `${value}px`}}
-                    ></div>
-                ))}
-                <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <div className="body">
+                <div className="navbar">
+                    <div id="buttons">
+                        <button id="generate" onClick={() => this.resetArray()}>Generate New Array</button>
+                        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                        <button onClick={() => this.quickSort()}>Quick Sort</button>
+                        <button onClick={() => this.heapSort()}>Heap Sort</button>
+                        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+                    </div>
+                </div>
+                <div className="array-container">
+                    {array.map((value, idx) => (
+                        <div
+                            className="array-bar"
+                            key={idx}
+                            style={{
+                                height: `${value}px`
+                            }}
+
+                        ></div>
+                    ))}
+                </div>
             </div>
+
         );
     }
 }
