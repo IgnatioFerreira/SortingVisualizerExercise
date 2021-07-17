2c89d6f3a571f7df1a7455e7cddbad26d66cb7f2
IgnatioFerreira <ignatio.ferreira@gmail.com>
2021-07-17 11:09:00 +0200

    Stuff

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -4,7 +4,7 @@ import { getMergeSortAnimations, bubbleSortAnimations } from "../SortingAlgorith
 
 const ANIMATION_SPEED_MS = 1;
 const NUMBER_OF_ARRAY_BARS = 50;
-const PRIMARY_COLOR = 'rgba(255, 128, 43, 0.748)';
+const PRIMARY_COLOR = '#f49f1c';
 const SECONDARY_COLOR = 'red';
 //TODO use quickmath to set the sorted time accoring to the blabla so that it is alway a constant maybe 5 secs
 
@@ -67,22 +67,24 @@ export default class SortingVisualizer extends React.Component {
     bubbleSort() {
         const animations = [];
         let bars = this.getCurrentLengths();
-        
+
         let i, j, temp, swapped;
         for (i = 0; i < bars.length; i++) {
             swapped = false;
-            for(j = 0; j < bars.length - i - 1; j++){
-                if(bars[j] > bars[j+1]){
+            for (j = 0; j < bars.length - i - 1; j++) {
+                if (bars[j] > bars[j + 1]) {
                     temp = bars[j];
-                    bars[j] = bars[j+1];
-                    bars[j+1] = temp;
+                    bars[j] = bars[j + 1];
+                    bars[j + 1] = temp;
                     swapped = true;
-                    animations.push([j, j+1,true]);
+                    animations.push([j, j + 1, true]);
+                    animations.push([j, j + 1, true]);
                 }
-                animations.push([j, j+1,false]);
-                
+                animations.push([j, j + 1, false]);
+                animations.push([j, j + 1, false]);
+
             }
-            if(!swapped){
+            if (!swapped) {
                 break;
             }
         }
@@ -90,19 +92,26 @@ export default class SortingVisualizer extends React.Component {
         // console.log(animations);
 
         for (let i = 0; i < animations.length; i++) {
+            const bubblespeed = 3;
+
             let bar1 = document.getElementById(`${animations[i][0]}`).style;
             let bar2 = document.getElementById(`${animations[i][1]}`).style;
-            
 
-            if(animations[i][2]){
+            const colorswap = i % 2 == 0;
+            if (animations[i][2] && colorswap) {
                 console.log(animations[i][2]);
                 setTimeout(() => {
                     let temp = bar1.height;
                     bar1.height = bar2.height;
                     bar2.height = temp;
-                }, i * ANIMATION_SPEED_MS * 5);
-            } else{
-
+                    // bar1.backgroundColor =  PRIMARY_COLOR;
+                    // bar2.backgroundColor = PRIMARY_COLOR;
+                }, i * ANIMATION_SPEED_MS * bubblespeed);
+            } else {
+                setTimeout(() => {
+                    bar1.backgroundColor = PRIMARY_COLOR;
+                    bar2.backgroundColor =  PRIMARY_COLOR;
+                }, i * ANIMATION_SPEED_MS * bubblespeed);
             }
         }
 
@@ -131,14 +140,22 @@ export default class SortingVisualizer extends React.Component {
     }
 
     testSort() {
-        for (let i = 0; i < 1; i++) {
-            const array = [];
-            for (let k = 0; k < NUMBER_OF_ARRAY_BARS; k++) {
-                array.push(randomIntFromInterval(5, 675));
+        console.log('alalalla');
+        for (let i = 0; i < 20; i++) {
+            let bar1 = document.getElementById(Math.floor(i/2)).style;
+            if (i % 2 == 0) {
+                setTimeout(() => {
+                    bar1.backgroundColor = SECONDARY_COLOR;
+                }, i * ANIMATION_SPEED_MS * 300);
+            } else {
+                setTimeout(() => {
+                    bar1.backgroundColor = 'blue';
+                }, i * ANIMATION_SPEED_MS * 300);
             }
-            let sort = bubbleSortAnimations(array);
-            console.log(sort);
+
         }
+
+
     }
 
     render() {
